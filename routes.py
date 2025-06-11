daa7fb48fde396038ee0386212530b5147042103
JulioFigueiredo <julioo.1amorim@gmail.com>
2025-06-11 14:11:00 -0300

    feat: add temperature route logic

--- routes.py
+++ routes.py
@@ -1,11 +1,25 @@
 from flask import render_template, request
+from forms import Temperature
 
 def configure_routes(app):
+    app.secret_key = 'secret'
     
     @app.route('/')
     def index():
-        render_template('index.html')
+        return render_template('index.html')
         
-    @app.route("/", methods=["GET", "POST"])    
-    def conversor_temperatura():
-        pass
+    @app.route("/temperature", methods=["GET", "POST"])    
+    def temperature_conversor():
+        form = Temperature()
+        if form.validate_on_submit():
+            value = form.value.data
+            type = form.type.data
+            
+            if type == 'c_to_f':
+                result = (value * 9/5) + 32
+            else:
+                result = (value - 32) * 5/9
+                
+            return render_template('temperature_conversor.html', form=form, result=result)
+        
+        return render_template('temperature_conversor.html', form=form)
